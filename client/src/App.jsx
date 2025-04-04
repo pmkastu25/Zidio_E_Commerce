e4ddef433bc6fc431619d273306c6d0caf0db9fb
pmkastu25 <pmkastu25@gmail.com>
2025-04-04 14:02:45 +0530

    frontend initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
+import Support from './pages/Support';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/support" element={<Support />} />
+      </Routes>
+  )
+}
+
+export default App
